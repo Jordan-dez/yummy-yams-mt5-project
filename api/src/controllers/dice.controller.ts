779565a62a85f6977145935fb83af26faadba74e
Jordan KAYA <jkaya@greenflex.com>
2024-05-28 22:33:10 +0200

    full projet

---
+++ api/src/controllers/dice.controller.ts
@@ -0,0 +1,47 @@
+import {Response} from "express";
+import {AuthRequest, UserResponse} from "../entities/types";
+import {ApiError} from "../errors";
+import User from "../entities/models/user.model";
+import {DiceService} from "../services/dice.service";
+import {UserService} from "../services/user.service";
+
+export async function throwDices(req: AuthRequest, res: Response) {
+    const authUserId = req.userId!;
+    const diceService = new DiceService();
+    const userService = new UserService();
+
+    const user = await userService.getUserById(authUserId);
+
+    if(!user) {
+        return res.status(403).json({
+            message:"User not found"
+        })
+    }
+
+    if (user.nbOfTriesLeft === 0) {
+        return res.status(403).json({
+            message:"You are not allowed to play anymore."
+        })
+    }
+
+    user.nbOfTriesLeft!-=1;
+
+    const shuffledDice = diceService.rollDices();
+
+    const pastriesWon = await diceService.getPastriesWonByRolledDices(shuffledDice);
+
+    if (pastriesWon.length > 0) {
+        user.nbOfTriesLeft = 0;
+
+        user.pastriesWon!.push(...pastriesWon.map(p => p.id.toString()));
+    }
+
+    await user.save();
+
+    const userWithPastriesWon = await userService.getUserById(authUserId);
+
+    return res.status(201).send({
+        dices: shuffledDice,
+        user: userService.buildUserResponse(userWithPastriesWon!)
+    });
+}
